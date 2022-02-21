a59df5c9cbaf592fb2e5c8fc99bfdceabe223109
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-02-21 23:15:18 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab6_2/Main.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitAPILab6_2.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab6_2
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Внимание!", "Сначала необходимо указать левой кнопкой мыши точку вставки экземпляра, а затем в появившемся окне выбрать семейство и уровень");
+
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPILab6_2/MainViewViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPILibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace RevitAPILab6_2
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+        public List<FamilySymbol> Furniture { get; } = new List<FamilySymbol>();
+        public List<Level> Levels { get; } = new List<Level> { };
+        public DelegateCommand SaveCommand { get; }
+        public XYZ InsertionPoint { get; }
+        public FamilySymbol SelectedFurniture { get; set; }
+        public Level SelectedLevel { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            Furniture = FurnitureUtils.GetFurniture(commandData);
+            Levels = LevelsUtils.GetLevels(commandData);
+            SaveCommand = new DelegateCommand(onSaveCommand);
+            InsertionPoint = SelectionUtils.GetPoint(_commandData, "Выберите точку", ObjectSnapTypes.Endpoints);
+        }
+        private void onSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (SelectedFurniture == null || SelectedLevel == null)
+                return;
+
+            var oLevel = (Level)doc.GetElement(SelectedLevel.Id);
+            var oFamSymb = (FamilySymbol)doc.GetElement(SelectedFurniture.Id);
+
+            FamilyInstanceUtils.CreateFamilyInstance(_commandData, oFamSymb, InsertionPoint, oLevel);
+
+            RaiseCloseRequest();
+        }
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
